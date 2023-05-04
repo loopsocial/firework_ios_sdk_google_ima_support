4049e0b95d52be2931f0ad5181b43df310bc6003
GraceFirework <110849574+GraceFirework@users.noreply.github.com>
2023-05-04 14:24:57 -0400

    feature/ISDK-984: Update package (#3)
    * update package
    
    * bump version
    
    * add dummy file

--- Package.swift
+++ Package.swift
@@ -9,13 +9,23 @@ let package = Package(
     ],
     products: [
         .library(name: "FireworkVideoGIMASupport",
-                 targets: ["FireworkVideoGIMASupport"])
+                 targets: ["FireworkVideoGIMASupport", "FireworkVideoGIMASupportDependencies"])
     ],
     dependencies: [
+        .package(
+            url: "https://github.com/loopsocial/GoogleAds-IMA-iOS-SDK.git",
+            .upToNextMajor(from: "3.16.3")
+        )
     ],
     targets: [
         .binaryTarget(name: "FireworkVideoGIMASupport",
-                      url: "https://github.com/loopsocial/firework_ios_sdk_google_ima_support/releases/download/v0.2.0/FireworkVideoGIMASupport-v0.2.0.xcframework.zip",
+                      url: "https://github.com/loopsocial/firework_ios_sdk_google_ima_support/releases/download/v0.3.0/FireworkVideoGIMASupport-v0.3.0.xcframework.zip",
                       checksum: "3062f1a9032f7587f7bb23f8c6903eb514b4a986a28ed24d0139b7a8d24a9083"),
+        .target(
+            name: "FireworkVideoGIMASupportDependencies",
+            dependencies: [
+                .product(name: "GoogleInteractiveMediaAds", package: "GoogleInteractiveMediaAds")
+            ]
+        )
     ]
 )
