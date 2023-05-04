8f64f90a94366d60cc8f562e2e2ac30cbf19c7a0
GraceFirework <110849574+GraceFirework@users.noreply.github.com>
2023-05-04 15:48:56 -0400

    feature/ISDK-984: Fix package name (#4)
    * update package
    
    * bump version
    
    * add dummy file
    
    * update package name

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .target(
             name: "FireworkVideoGIMASupportDependencies",
             dependencies: [
-                .product(name: "GoogleInteractiveMediaAds", package: "GoogleInteractiveMediaAds")
+                .product(name: "GoogleInteractiveMediaAds", package: "GoogleAds-IMA-iOS-SDK")
             ]
         )
     ]
